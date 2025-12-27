d2c2d0fb670c266740ebe0310b5e843c58f220ea
Romeiro <theromeirofernandes@gmail.com>
2025-12-27 12:26:16 +0530

    Initialized frontend with ShadCN

---
+++ frontend/src/pages/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            <h1 className="text-4xl font-bold mb-4">Welcome to Tech Fiesta</h1>
+            <p className="text-lg text-gray-700">Your gateway to the latest in technology events.</p>
+        </div>
+    </>
+  )
+}
+
+export default Landing
